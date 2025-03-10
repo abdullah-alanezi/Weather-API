69eb08f6170bdfb8ef4c54cd48c06e1f86ad615e
abdullah <abdullah.alanezi.23@gmail.com>
2025-03-10 04:04:25 +0300

    change in api key to get real data

--- weather/city/views.py
+++ weather/city/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render
 import requests
 # Create your views here.
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def weather_city(request):
-    city = "Hail"
+    city = 0
     if request.method == "POST":
         city = request.POST['city']
     
-    url = f"https://wttr.in/{city}?format=j1"
+    # Your OpenWeatherMap API Key
+    api_key = os.getenv('API_KEY')
+    
+    # OpenWeatherMap API URL
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     
     response = requests.get(url)
     data_weather = response.json()
 
-    # استخراج بيانات الطقس المطلوبة
-    current_condition = data_weather["current_condition"][0]  # الطقس الحالي
-    weather_info = {
-        "city": city,
-        "temperature": current_condition["temp_C"],  # درجة الحرارة المئوية
-        "humidity": current_condition["humidity"],  # الرطوبة
-        "description": current_condition["weatherDesc"][0]["value"],  # وصف الطقس
-        "wind_speed": current_condition["windspeedKmph"],
-        "icon": current_condition["weatherIconUrl"][0]["value"],  # رابط الأيقونة
-        "data_weather": data_weather 
-    }
-
+    if data_weather["cod"] != 200:
+        weather_info = {"error": "City not found"}
+    else:
+        # استخراج بيانات الطقس المطلوبة
+        weather_info = {
+            "city": city,
+            "temperature": data_weather["main"]["temp"],  # درجة الحرارة المئوية
+            "humidity": data_weather["main"]["humidity"],  # الرطوبة
+            "description": data_weather["weather"][0]["description"],  # وصف الطقس
+            "wind_speed": data_weather["wind"]["speed"],  # سرعة الرياح
+            "icon": f"http://openweathermap.org/img/wn/{data_weather['weather'][0]['icon']}@2x.png",  # رابط الأيقونة
+            "data_weather": data_weather 
+        }
 
     return render(request, "city/weather.html", {"weather": weather_info})
+
 
